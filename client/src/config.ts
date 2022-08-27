1807e9df85dcce0b8c30754dc0c9724296fae41f
dims3753 <dimeji3753@gmail.com>
2022-08-27 21:52:34 +0100

    Added screenshot of success through Postman

--- client/src/config.ts
+++ client/src/config.ts
@@ -2,7 +2,7 @@ const apiId = '';
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
 
 export const authConfig = {
-  domain: '',
-  clientId: '',
+  domain: 'dev-fq4c-hts.us.auth0.com',
+  clientId: 'GWaulq774pQVFEPHffLE3FGTWZpQVNkT',
   callbackUrl: 'http://localhost:3000/callback'
 };
